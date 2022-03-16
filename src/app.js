cc255f55a03e51f55cddfce9974241d929f169ab
Maga8005 <maleja8005@gmail.com>
2022-03-15 20:33:13 -0500

    PORT

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require ('express');
 const path = require('path'); //module nativo node
 const app = express(); // Ejecuto express
-const port = 3000; //Coloco el puerto x en una constante
+const PORT = process.env.PORT || 3000; //Coloco el puerto x en una constante
 
 
 app.use(express.static(path.join(__dirname,'../public'))); // __dirname (es la ruta absoluta de la carpeta raiz) ../ (me devuelvo un nivel). App.use, permite acceder a todos los archivos públicos.
 
-app.listen(port, () => console.log('Servidor corriendo')); // Inicializo el servidor.
+app.listen(PORT, () => console.log('Servidor corriendo')); // Inicializo el servidor.
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html'))); //Devuelvo el archivo home.html al cliente, con ruta absoluta. Se utiliza para todos los archivos que quiero devolver.
 app.get('/register.html',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
